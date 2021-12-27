73afb78ade4cf44fc49685eb4f42cac908180991
Raaydon <billjrimell@gmail.com>
2021-12-27 22:48:27 +0000

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
 ReactDOM.render(
   <React.StrictMode>
-    <StyledEngineProvider>
-      <CssBaseline />
       <App />
-    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,10 @@
+import React from "react";
+import './styles.css';
+
 const Dashboard = () => {
   return (
     <div>
-      
+      <div className="elipse"></div>
     </div>
   );
 }
