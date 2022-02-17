5055d8c99272785c2b0dbf756ca0eb8b57b8587b
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-17 21:04:51 +0000

    dura auto-backup

--- src/components/Multiplayer/PlayerListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PlayerListItem(props) {
-    return (
-        <div className="player-list-item">
-            <div className="player-list-item-name">{props.user}</div>
-        </div>
-    )
-}
