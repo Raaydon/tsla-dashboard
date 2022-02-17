6b3517615ed28c282847d1bf8f4575e5924d4b13
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-17 21:09:10 +0000

    dura auto-backup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
-import { useModal } from "react-hooks-use-modal";
 
 import Nav from "./components/Nav/MainNav";
 import Dashboard from './components/Dashboard/Dashboard';
