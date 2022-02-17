29fd53f67e21b637c360d44f59e273a2029752d0
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-17 21:10:27 +0000

    dura auto-backup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 
-import Nav from "./components/Nav/Nav";
 import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
