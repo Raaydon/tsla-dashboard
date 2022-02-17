20bc7b42008d045cc7ccba11e2dc5529114ee1ae
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-17 21:08:30 +0000

    dura auto-backup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import { useModal } from "react-hooks-use-modal";
 
-import MainNav from "./components/Nav/MainNav";
+import Nav from "./components/Nav/MainNav";
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
 	return (
-	<MainNav />)
+		<div className="App">
+			<Nav />
+			<Dashboard />
+		</div>
+	)
 }
 
 export default App;
