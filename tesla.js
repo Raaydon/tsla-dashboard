f9ec693f1de357511aeb268e2076d737de5dbf8b
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-18 21:16:29 +0000

    dura auto-backup

--- tesla.js
+++ tesla.js
@@ -1,7 +1,7 @@
 var tjs = require('teslajs');
 
-
 const authToken = process.env.REACT_APP_TOKEN;
+
 var options = { authToken: authToken };
 tjs.vehicle(options, function (err, vehicle) {
     console.log("Vehicle " + vehicle.vin + " is: " + vehicle.state);
