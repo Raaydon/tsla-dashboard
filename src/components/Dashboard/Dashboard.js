f3fe2d77dc10b20ee2056b13e2bf05fe4944f683
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-18 19:58:30 +0000

    update

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import tjs  from 'teslajs';
 
 export default function Dashboard() {
-    console.log(process.env)
+	const token = process.env.REACT_APP_TOKEN;
+	var options = { authToken: result.authToken };
+    tjs.vehicle(options, function (err, vehicle) {
+        console.log("Vehicle " + vehicle.vin + " is: " + vehicle.state);
+    });
 	return (
 		<div>
 			<h1>Dashboard</h1>
