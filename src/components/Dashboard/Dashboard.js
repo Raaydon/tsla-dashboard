3e1350631158e32e233613c9dbfc3c5563f9c27d
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-18 21:10:40 +0000

    dura auto-backup

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
-import tjs  from 'teslajs';
+import tjs from 'teslajs';
 
 export default function Dashboard() {
-	const token = process.env.REACT_APP_TOKEN;
-	var options = { authToken: result.authToken };
+	const authToken = process.env.REACT_APP_TOKEN;
+	var options = { authToken: authToken };
     tjs.vehicle(options, function (err, vehicle) {
         console.log("Vehicle " + vehicle.vin + " is: " + vehicle.state);
     });
