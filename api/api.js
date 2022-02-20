17072e9623db846136b6fbf41b8f26be7c9fcb89
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-20 19:31:34 +0000

    dura auto-backup

--- api/api.js
+++ api/api.js
@@ -29,16 +29,21 @@ app.get('/vehicles', async (req, res) => {
 });
 
 app.get('/vehicle/', async (req, res) => {
-    const accessToken = req.headers.authorization.replace(/^Bearer /, '');
-    if (!accessToken) res.sendStatus(403);
-    const response = await axios.get(`${baseUrl}/api/1/vehicles/`, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        }
+    try {
+        const accessToken = req.headers.authorization.replace(/^Bearer /, '');
+        if (!accessToken) res.sendStatus(403);
+        const response = await axios.get(`${baseUrl}/api/1/vehicles/`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        });
+        const id = response?.data?.response[0]?.id;
+        res.send(JSON.stringify(id));
     });
-    const id = response?.data?.response[0]?.id;
-    res.send(JSON.stringify(id));
-});
+    } catch (error) {
+        console.log('error at vehicle');
+    }
+
 
 app.get('/vehicle/:id/state/', async (req, res) => {
     const accessToken = req.headers.authorization.replace(/^Bearer /, ''),
