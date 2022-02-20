7f4439ae15110cae8b33b88c6ffb074a346f84d4
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-02-20 20:26:52 +0000

    dura auto-backup

--- api/api.js
+++ api/api.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
 
 app.get("/vehicles", async (req, res) => {
 	const accessToken = req.headers.authorization.replace(/^Bearer /, "");
-	if (!accessToken || accessToken === null) {
+	if (!accessToken) {
 		res.sendStatus(403);
 	} else {
 		const response = await axios.get(`${baseUrl}/api/1/vehicles`, {
