f1ca46c73a4f9ae8f6f6c7f761e5f8dc3b7851e5
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-03-24 20:59:12 +0000

    switch to vite

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), VitePWA({
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Tesla Dashboard',
+      short_name: 'Tesla Dash',
+      description: 'Tesla Dashboard',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+    
+  })],
+	publicDir: "./public",
+	base: "/",
+	build: {
+		outDir: "build",
+		cssCodeSplit: true,
+		sourcemap: true,
+	},
+});
