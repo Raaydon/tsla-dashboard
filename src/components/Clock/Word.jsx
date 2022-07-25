70072b0b3247c64409225509ec2647372f80f56b
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-07-25 10:27:23 +0100

    dura auto-backup

--- src/components/Clock/Word.jsx
+++ src/components/Clock/Word.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const Word = ({ value, hidden = false }) => {
-  const getStyle = ()=> {
-    return {
-      visibility:  hidden ? 'hidden' : 'visible'
-    }
-  }
-  return (
-    <div className='digital'>
-      <p>{value}</p>
-      <p style={getStyle()}>{value}</p>
-    </div>
-  )
-}
+	const getStyle = () => {
+		return {
+			visibility: hidden ? "hidden" : "visible",
+		};
+	};
+	return (
+		<div className="digital">
+			<p>{value}</p>
+			<p style={getStyle()}>{value}</p>
+		</div>
+	);
+};
