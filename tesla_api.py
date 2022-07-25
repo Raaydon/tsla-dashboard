070216607d0d4d1681645399b5d2e9305d3ab83c
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-07-25 11:00:48 +0100

    dura auto-backup

--- tesla_api.py
+++ tesla_api.py
@@ -7,7 +7,7 @@ from flask_cors import CORS, cross_origin
 
 email = os.getenv("REACT_APP_EMAIL")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
